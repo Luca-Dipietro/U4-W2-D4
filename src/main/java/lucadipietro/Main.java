ef993abb12447a93e1b00f4499dd32c0c4257ac9
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-05-30 17:22:29 +0200

    Finish ex 2

--- src/main/java/lucadipietro/Main.java
+++ src/main/java/lucadipietro/Main.java
@@ -8,8 +8,10 @@ import lucadipietro.entities.Product;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -43,11 +45,40 @@ public class Main {
 
         products.forEach(System.out::println);
 
+        System.out.println();
+
+        System.out.println("Lista Ordini");
+
         Supplier<Order> orderSupplier = () -> {
             LocalDate orderDate = LocalDate.now().minusDays(faker.number().numberBetween(1, 30));
-            LocalDate deliveryDate = orderDate.plusDays(faker.number().numberBetween(1,10));
-            List<Product> orderProducts =
-            return new Order(faker.number().randomNumber(),faker.lorem().word(),orderDate,deliveryDate,
+            LocalDate deliveryDate = orderDate.plusDays(faker.number().numberBetween(1, 10));
+            List<Product> orderProducts = new ArrayList<>();
+            int numRandomProducts = faker.number().numberBetween(1, 3);
+            for (int i = 0; i < numRandomProducts; i++) {
+                int randomIndex = faker.number().numberBetween(0, products.size() - 1);
+                orderProducts.add(products.get(randomIndex));
+            }
+            return new Order(faker.number().randomNumber(), faker.lorem().word(), orderDate, deliveryDate, orderProducts, costumers.get(faker.number().numberBetween(0, costumers.size())));
+        };
+
+        List<Order> orders = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            orders.add(orderSupplier.get());
         }
+
+        orders.forEach(System.out::println);
+
+        System.out.println();
+
+        System.out.println("Esercizio 1");
+
+        Map<Costumer, List<Order>> ordersByCostumer = orders.stream()
+                .collect(Collectors.groupingBy(Order::getCostumer));
+
+        ordersByCostumer.forEach((costumer, ordersList) -> {
+            System.out.println(costumer);
+            ordersList.forEach(System.out::println);
+        });
     }
 }
